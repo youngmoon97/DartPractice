094e2301f6b12ee62b751d310614fd0cd4e87131
youngmoon97 <whdudans12@gmail.com>
2023-03-06 16:25:48 +0900

    dart문법

---
+++ s05_list.dart
@@ -0,0 +1,78 @@
+/*
+
+  List 타입의 대표적 메소드
+  add: 요소를 추가한다.
+  remove: 요소를 제거한다.
+  clear: 모든 요소를 제거한다.
+  insert: 인덱스를 통해 요소를 추가한다.
+  indexOf: 요소의 인덱스를 반환한다.
+  sort: 요소를 정렬한다.
+  shuffle: 요소를 랜덤하게 섞는다.
+  join: 요소를 문자열로 합친다.
+  split: 문자열을 요소로 분리한다.
+  forEach: 요소를 반복한다.
+  map: 요소를 변환한다.
+  where: 조건에 맞는 요소를 반환한다. (filter)
+  any: 조건에 맞는 요소가 있는지 확인한다.
+  every: 모든 요소가 조건에 맞는지 확인한다.
+  isEmpty: 요소가 비어있는지 확인한다.
+  isNotEmpty: 요소가 비어있지 않은지 확인한다.
+  contains: 요소가 포함되어 있는지 확인한다.
+
+
+  toList:
+  map, where 등은 Iterable로 반환되기 때문에 
+  다시 List로 만들어야한다. 
+  list.where().toList();
+
+ */
+
+
+
+void main(List<String> args) {
+  List tempList1 = [3, 2, 1];
+
+  // 문제 1. add를 이용해서 4를 추가하고 출력하시오.
+  tempList1.add(4);
+  print(tempList1);
+  // 문제 2. remove를 이용해서 2를 제거하고 출력하시오.
+  tempList1.remove(2);
+  print(tempList1);
+  // 문제 3. insert를 이용해서 1번 인덱스에 4를 추가하고 출력하시오.
+  tempList1.insert(1, 4);
+  print(tempList1);
+  // 문제 4. indexOf를 이용해서 3의 인덱스를 출력하시오.
+  print(tempList1.indexOf(3));
+  // 문제 5. sort를 이용해서 tempList1을 정렬하고 출력하시오.
+  // (인터넷 검색하여 매개변수가 어떤 것이 들어가야 하는지 확인)
+  tempList1.sort();
+  print(tempList1);
+  // 문제 6. shuffle를 이용해서 tempList1을 랜덤하게 섞고 출력하시오.
+  tempList1.shuffle();
+  print(tempList1);
+  // 문제 7. join을 이용해서 tempList1을 문자열로 합치고 출력하시오.
+  print(tempList1.join());
+  // 문제 8. split을 이용해서 문제7번의 문자열을 분리하고 출력하시오.
+  String tempString = tempList1.join();
+  print(tempString);
+  print(tempString.split(""));
+  // 문제 9. map을 이용해서 tempList1의 요소를 2배로 만들고 출력하시오.
+  // (인터넷 검색하여 매개변수가 어떤 것이 들어가야 하는지 확인)
+  print(tempList1.map((e) => e*2));
+  // 문제 10. where를 이용해서 tempList1의 짝수만 출력하시오.
+  // (인터넷 검색하여 매개변수가 어떤 것이 들어가야 하는지 확인)
+  print(tempList1.where((element) => element%2==0));
+  // 문제 11. forEach를 이용해서 tempList1의 요소를 반복하고 출력하시오.
+  // (인터넷 검색하여 매개변수가 어떤 것이 들어가야 하는지 확인)
+  tempList1.forEach((element) { 
+    print(element);
+  });
+  // 문제 12. any를 이용해서 tempList1에 3이 있는지 확인하고 출력하시오.
+  print(tempList1.any((element) => element==3));
+  // 문제 13. every를 이용해서 tempList1의 모든 요소가 3보다 작은지 확인하고 출력하시오.
+  print(tempList1.every((element) => element<3));
+  // 문제 14. isEmpty를 이용해서 tempList1이 비어있는지 확인하고 출력하시오.
+  print("문제14 :  ${tempList1.isEmpty}");
+  // 문제 15. contains를 이용해서 tempList1에 3이 있는지 확인하고 출력하시오.
+  print(tempList1.contains(3));
+}
---
+++ s08_null_safety.dart
@@ -0,0 +1,35 @@
+/*
+
+  null safety
+
+  https://dart.dev/null-safety
+
+  null이 들어갈 수 있는 변수를 선언할 때는 ?를 붙여서 선언한다.
+
+  ?가 붙은 변수는 값을 할당해주지 않으면 null로 초기화 된다.
+
+  int? exampleNum = null;
+  String? exampleStr = null;
+
+  Object 타입은 null을 넣으려면 ?를 붙여야 한다.
+
+  dynamic 타입은 null을 넣을 수 있다.
+  
+*/
+
+void main(List<String> args) {
+  // 문제 1. null이 들어갈 수 있는 정수형 변수 tempInt1을 선언하고 출력하시오.
+  int? tempInt1 ;
+  print(tempInt1);
+  // 문제 2. null이 들어갈 수 없는 정수형 변수 tempInt2를 선언하고 출력하시오.
+  int tempInt2 ;
+ // print(tempInt2);
+  // 문제 3. null이 들어갈 수 있는 문자열 변수 tempStr1에 null을 대입하고 출력하시오.
+  String? tempStr1 ;
+  print(tempStr1);
+
+  // 문제 4. null이 들어갈 수 없는 문자열 변수 tempStr2에 null을 대입하고 출력하시오.
+  String tempStr2 ;
+  //print(tempStr2);
+
+}
